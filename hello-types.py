dd704f82828d9bc87848409532a111098708d590
psvehla <psvehla@hotmail.com>
2024-01-24 01:00:50 +1100

    Added some type hints.

--- hello-types.py
+++ hello-types.py
@@ -2,9 +2,16 @@
 """Type hints."""
 
 
-def headline(text, align=True):
+def headline(text: str, align: bool = True) -> str:
     """Turn text into a headline."""
     if align:
         return f"{text.title()}\n{'-' * len(text)}"
     else:
         return f" {text.title()} ".center(50, "o")
+
+
+print(headline("python type checking"))
+print()
+print(headline("python type checking", align=False))
+print()
+print(headline("python type checking", align="centre"))
