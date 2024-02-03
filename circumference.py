7f8ea705520286ecb275a6b37b5fff91f852129e
psvehla <psvehla@hotmail.com>
2024-02-03 15:49:25 +1100

    A couple of more examples.

---
+++ circumference.py
@@ -0,0 +1,15 @@
+"""Calculates the circumference of a circle."""
+import math
+
+
+def circumference(radius: float) -> float:
+    """Calculate the circumference of a circle."""
+    return 2 * math.pi * radius
+
+
+pi: float = 3.14159265
+
+
+def circumference2(radius: float) -> float:
+    """Calculate the circumference of a circle."""
+    return 2 * math.pi * radius
---
+++ reveal.py
@@ -0,0 +1,9 @@
+"""Annotation reveal examples."""
+
+import math
+
+reveal_type(math.pi)
+
+radius = 1
+circumference = 2 * math.pi * radius
+reveal_locals()
