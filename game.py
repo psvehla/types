032c14923861dd39f1baa773c8d633c112a83f1c
psvehla <psvehla@hotmail.com>
2024-02-05 17:26:28 +1100

    Type aliases.

--- game.py
+++ game.py
@@ -2,21 +2,24 @@
 # game.py
 
 import random
+from typing import List, Tuple
+
+Card = Tuple[str, str]
+Deck = List[Card]
 
 SUITS = "♠ ♡ ♢ ♣".split()
 RANKS = "2 3 4 5 6 7 8 9 10 J Q K A".split()
 
 
-def create_deck(shuffle=False):
+def create_deck(shuffle: bool = False) -> List[Tuple[str, str]]:
     """Create a new deck of 52 cards."""
     deck = [(s, r) for r in RANKS for s in SUITS]
-    print(deck)
     if shuffle:
         random.shuffle(deck)
     return deck
 
 
-def deal_hands(deck):
+def deal_hands(deck: Deck) -> Tuple[Deck, Deck, Deck, Deck]:
     """Deal the cards in the deck into four hands."""
     return (deck[0::4], deck[1::4], deck[2::4], deck[3::4])
 
